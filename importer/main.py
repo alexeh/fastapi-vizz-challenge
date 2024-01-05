25da1f74628ab85a45b23acb4905fa92cbb9eaa6
alexeh <alexurola@gmail.com>
2024-01-05 12:57:12 +0300

    integrate dynamoDB for writes

--- importer/main.py
+++ importer/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, File, UploadFile, Depends
 from a2wsgi import ASGIMiddleware
-from db.database import get_mongo_client
+from db.database import get_mongo_client, get_dynamodb_client
 from middlewares.file_validator import file_validator
 from middlewares.request_validator import RequestValidatorMiddleware
 from utils.csv_parser import parse_csv_file
@@ -12,7 +12,8 @@ app.add_middleware(RequestValidatorMiddleware)
 
 
 
-client = get_mongo_client()
+#client = get_mongo_client()
+client = get_dynamodb_client()
 
 # Ensure the temp_files directory exists
 temp_files_dir = Path(os.getcwd() + "/temp_files")
@@ -29,14 +30,24 @@ async def create_upload_file(file: UploadFile = File(...)):
 
     parsed_data = parse_csv_file(file_path)
 
-    # Insert the data into the MongoDB collection
-    result = client.insert_many(parsed_data)
+    # # Insert the data into the MongoDB collection
+    # result = client.insert_many(parsed_data)
+    counter = 0
+    result = []
+    with client.batch_writer() as batch:
+        for item in parsed_data:
+            print('each item', item)
+            counter += 1
+            print('counter', counter)
+            batch.put_item(item)
+            result.append(item)
 
     # Remove the uploaded file from disk
     os.remove(file_path)
 
     # Return a JSON response with the number of documents inserted
-    return {"message": f"{len(result.inserted_ids)} documents inserted."}
+
+    return {"message": f"{len(result)} documents inserted."}
 
 
 @app.get("/upload/ping")
