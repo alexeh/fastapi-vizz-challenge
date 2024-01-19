786bb896ec9f24b7c0fa636cc047802ebb522103
alexeh <alexurola@gmail.com>
2024-01-19 09:26:13 +0300

    setup deployment in GH actions

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,3 +7,9 @@ set :ssh_options, {
   forward_agent: false,
   auth_methods: %w(publickey)
 }
+
+after 'deploy:starting', 'exporter:deploy'
+after 'exporter:deploy', 'importer:deploy'
+after 'importer:deploy', 'deploy:restart_services'
+after 'deploy:restart_services', 'deploy:health_exporter'
+after 'deploy:health_exporter', 'deploy:health_importer'
